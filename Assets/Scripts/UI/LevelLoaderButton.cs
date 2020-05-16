858570678fcf1fea2fbaa37539bbd776fad42d96
Jeff Kapochus <jeff.kapochus@gmail.com>
2020-05-16 16:36:01 -0400

    Tuts

---
+++ Assets/Scripts/UI/LevelLoaderButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoaderButton : MonoBehaviour
+{
+    public void LoadLevel(string toLoad)
+    {
+        LevelManager.Instance.LoadLevel(toLoad);
+    }
+}
