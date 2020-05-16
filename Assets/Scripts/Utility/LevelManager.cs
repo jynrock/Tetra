d3973def3d331ede756baa47c94efd7ab5d1e089
Jeff Kapochus <jeff.kapochus@gmail.com>
2020-05-16 11:49:15 -0400

    Load Screen Finished!

--- Assets/Scripts/Utility/LevelManager.cs
+++ Assets/Scripts/Utility/LevelManager.cs
@@ -2,23 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static LevelManager _instance;
+    public static LevelManager Instance
     {
-        
+        get { return _instance; }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private Animator cardAnimator;
+    [SerializeField]
+    private Slider loadBar;
+
+    void Awake()
     {
-        
+        if (_instance == null || _instance != this)
+        {
+            _instance = this;
+        }
     }
 
     public void LoadLevel(string levelName)
     {
-        SceneManager.LoadSceneAsync(levelName);
+        StartCoroutine(LoadLevelAsync(levelName));
+    }
+
+    private IEnumerator LoadLevelAsync(string levelName)
+    {
+        loadBar.value = 0;
+        cardAnimator.StopPlayback();
+        animator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1.0f);
+        AsyncOperation op = SceneManager.LoadSceneAsync(levelName);
+        while (!op.isDone)
+        {
+            loadBar.value = Mathf.Clamp01(op.progress / 0.9f);
+            yield return null;
+        }
+        animator.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1.0f);
+        cardAnimator.StartPlayback();
     }
 }
