67f0cc91269f640f726aaf555b61804b171c31a1
Jeff Kapochus <jeff.kapochus@gmail.com>
2020-07-03 20:03:30 -0400

    Globalized GetRandom AI Functions, Implemented Chance to Not Play Card On Turn

--- Assets/Scripts/Gameplay/Player/AI/AIBase.cs
+++ Assets/Scripts/Gameplay/Player/AI/AIBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -24,6 +25,52 @@ public abstract class AIBase : MonoBehaviour
     public void SetPlayCardEvent(BattlecardTileEvent _event)
     {
         tryPlayCardEvent = _event;
+    }
+
+    protected Tile GetRandomTile()
+    {
+        Tile result = null;
+
+        List<int> tilesTried = new List<int>();
+        do
+        {
+            int i = 0;
+            do
+            {
+                i = Random.Range(0, board.tilesList.Count);
+            }
+            while (tilesTried.Contains(i));
+
+            tilesTried.Add(i);
+            result = board.tilesList[i];
+        }
+        while (result.card != null || result.blocker != null);
+
+        return result;
+    }
+
+    protected BattleCard GetRandomCardFromHand()
+    {
+        BattleCard result = null;
+
+        if(player.hand.Count > 0)
+        {
+            int i = Random.Range(0, player.hand.Count);
+            result = player.hand[i];
+        }
+
+        return result;
+    }
+
+    protected BattleCard GetRandomPlayerOwnedCardOnBoard()
+    {
+        List<BattleCard> test = board.cardsInPlay.Where(c => c.currentOwner = player).ToList();
+        if(test.Count > 0)
+        {
+            int random = Random.Range(0, test.Count);
+            return test[random];
+        }
+        return null;
     }
 
     public abstract void TakeTurn();
--- Assets/Scripts/Gameplay/Player/AI/RandoAI.cs
+++ Assets/Scripts/Gameplay/Player/AI/RandoAI.cs
@@ -6,53 +6,17 @@ public class RandoAI : AIBase
 {
     public override void TakeTurn()
     {
-        Tile t = GetRandomTile();
-        BattleCard c = GetRandomCardFromHand();
-        if(t != null && c != null)
+        int oneInTen = Random.Range(0, 10);
+        if(player.hand.Count >= 5 || oneInTen != 9)
         {
-            //int fiftyFifty = Random.Range(0, 2);
-            int fiftyFifty = 1;
-            if(fiftyFifty == 1)
+            Tile t = GetRandomTile();
+            BattleCard c = GetRandomCardFromHand();
+            if(t != null && c != null)
             {
                 tryPlayCardEvent.Raise(new BattlecardTileEventData(c, t));
             }
         }
         EndTurn();
-    }
-
-    private Tile GetRandomTile()
-    {
-        Tile result = null;
-
-        List<int> tilesTried = new List<int>();
-        do
-        {
-            int i = 0;
-            do
-            {
-                i = Random.Range(0, board.tilesList.Count);
-            }
-            while (tilesTried.Contains(i));
-
-            tilesTried.Add(i);
-            result = board.tilesList[i];
-        }
-        while (result.card != null || result.blocker != null);
-
-        return result;
-    }
-
-    private BattleCard GetRandomCardFromHand()
-    {
-        BattleCard result = null;
-
-        if(player.hand.Count > 0)
-        {
-            int i = Random.Range(0, player.hand.Count);
-            result = player.hand[i];
-        }
-
-        return result;
     }
 
     private void EndTurn()
