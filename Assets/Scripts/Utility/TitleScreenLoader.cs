7a83aa71ac433200f0ee2533aba163f7f926d9c2
Jeff Kapochus <jeff.kapochus@gmail.com>
2020-10-23 21:24:25 -0400

    New Start Screen, UI Beginnings

--- Assets/Scripts/Utility/TitleScreenLoader.cs
+++ Assets/Scripts/Utility/TitleScreenLoader.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using UnityEngine.SceneManagement;
 
 public class TitleScreenLoader : MonoBehaviour
 {
