c12775bcd5e8cd6cb5f6a3c9101c27d0ac4500c7
Jeff Kapochus <jeff.kapochus@gmail.com>
2020-10-25 11:28:43 -0400

    Initial PlasticSCM Commit

---
+++ Assets/Scripts/SceneElements/AnimationRandomizer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRandomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator anim = GetComponent<Animator> ();
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo (0);
+        anim.Play (state.fullPathHash, -1, Random.Range(0f,1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
